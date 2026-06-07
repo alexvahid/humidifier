3adc566a11919281999a8a4b07a9688e4b8eaee1
alexvahid <avahid@bu.edu>
2026-06-07 19:56:24 -0400

    fixing bash_profile for slow pi

--- bash_profile.sh
+++ bash_profile.sh
@@ -1,5 +1,5 @@
 source /home/pi/humidifier/humidifierenv/bin/activate
 cd /home/pi/humidifier
-git pull --timeout=5 2>/dev/null || true
+git pull --timeout=10 2>/dev/null || true
 cp ./bash_profile.sh ~/.bash_profile
 python3 /home/pi/humidifier/humidifier.py &
